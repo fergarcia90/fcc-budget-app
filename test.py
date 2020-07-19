182510fbaadbf9d73106debf42630540b6eeb115
Fernando Garcia <fer.garcia90@gmail.com>
2020-07-19 17:56:10 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+test_str = '''
+This is is a multiline string Thsi djjjsldjsljsdljksdjsjs dkfjl
+How do you do    sjfdlksjdlksdj					ksldlkdfslkf;ds 		;lsk;dks
+ldslkdjfds 		lkdjflksdfj
+Shire baggins
+'''
+
+print(test_str)
